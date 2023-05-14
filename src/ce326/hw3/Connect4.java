78314a1fe982e048085d21d791fb1f082d817411
Avramidis <pravramidis@gmail.com>
2023-05-14 21:24:52 +0300

    Add taskbar icon

--- src/ce326/hw3/Connect4.java
+++ src/ce326/hw3/Connect4.java
@@ -30,6 +30,8 @@ public class Connect4 {
         HistoryPanel.createHistoryDirectory();
 
         JFrame Connect4 = new JFrame();
+        Image whiteIcon = Toolkit.getDefaultToolkit().getImage("icons/taskBarIcon.png");
+        Connect4.setIconImage(whiteIcon);
         Connect4.setSize(WIDTH, HEIGHT);
         Connect4.setTitle("Connect4");
         Connect4.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
